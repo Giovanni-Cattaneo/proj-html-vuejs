cd1316f0ad4cd6c954a1ab788c6f3db984630064
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-04-08 16:02:27 +0200

    add jumbotron

--- src/App.vue
+++ src/App.vue
@@ -21,8 +21,15 @@ export default {
   <!-- /#site_header -->
 
   <main id="site_main">
-    <div class="jumbotron">
-      <img src="." alt="">
+    <div class="jumbotron d-flex">
+      <img src="./assets/slider.jpg" alt="">
+      <div class="jumbotron_message">
+        <p>Welcome to our Site</p>
+        <h1>Our special movies</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam maxime facilis doloribus voluptatibus enim
+          cupiditate dolore corrupti libero autem veritatis.</p>
+        <button>Read More</button>
+      </div>
     </div>
   </main>
   <!-- /#site_main -->
@@ -34,4 +41,28 @@ export default {
 
 </template>
 
-<style></style>
+<style>
+.jumbotron {
+  justify-content: center;
+  position: relative;
+
+  & img {
+    width: 1920px;
+    margin: auto;
+  }
+
+  .jumbotron_message {
+    position: absolute;
+    width: 300px;
+    text-align: center;
+    top: 5rem;
+    right: 10rem;
+
+    & button {
+      background-color: #13be13;
+      border: none;
+      padding: 0.5rem 0.8rem;
+    }
+  }
+}
+</style>
