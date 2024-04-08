d4ded848debd0cfcdb235b531fa2a2b159446274
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-04-08 16:30:40 +0200

    add newMovie section

--- src/App.vue
+++ src/App.vue
@@ -31,6 +31,19 @@ export default {
         <button>Read More</button>
       </div>
     </div>
+
+    <div class="card_section">
+      <div class="new_movie d-flex">
+        <div class="new_movie_text d-flex">
+          <strong>New Movie</strong>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, illum!</p>
+        </div>
+        <div class="arrows d-flex">
+          <button><i class="fa-solid fa-angles-left"></i></button>
+          <button><i class="fa-solid fa-angles-right"></i></button>
+        </div>
+      </div>
+    </div>
   </main>
   <!-- /#site_main -->
 
@@ -62,6 +75,39 @@ export default {
       background-color: #13be13;
       border: none;
       padding: 0.5rem 0.8rem;
+    }
+  }
+}
+
+.card_section {
+  margin-top: 3rem;
+
+  .new_movie {
+    width: 100%;
+    justify-content: space-between;
+
+    .arrows {
+      align-items: center;
+      gap: 1rem;
+
+      & button {
+        color: #13be13;
+        background-color: black;
+        border-radius: 50%;
+        border: none;
+        font-size: 1.4rem;
+        border: 1px solid white;
+        padding: 0.2rem 0.3rem;
+      }
+
+    }
+
+    .new_movie_text {
+      width: 90%;
+      flex-direction: column;
+      border-left: 2.5px solid #13be13;
+      border-bottom: 0.1px solid darkgrey;
+      padding-left: 0.3rem;
     }
   }
 }
